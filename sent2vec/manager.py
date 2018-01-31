e66837a1e58a30be41773d79f6720e6c58869a64
Daniel Weibel <danielmweibel@gmail.com>
2018-01-30 23:10:58 -0500

    Two different kNN algorithms
    Now, Sent2VecManager.query(sentence, k, type="dist"), where 'type' can
    be "dist" or "score".
    
    In the case of "dist", scipy.spatial.distance.cdist is used to determine
    distance between vectors (smaller distance is nearer).
    
    In the case of "score", another approach using numpy.dot is used to
    determine a score for each vector pair (higher is nearer).
    
    The latter is the approach used in the original skip-thoughts implementation,
    in particular, function "nn" in "skipthoughts.py".
    
    The code is working.

--- sent2vec/manager.py
+++ sent2vec/manager.py
@@ -1,5 +1,6 @@
 import numpy
 from scipy.spatial.distance import cdist
+from scipy.linalg import norm
 import skipthoughts.skipthoughts as skipthoughts
 from tokenizer import SentenceTokenizer
 import util
@@ -59,14 +60,59 @@ class Sent2VecManager:
         self.read_string(f.read())
         f.close()
 
-    def query(self, sentence, k):
+    def query(self, sentence, k, type="dist"):
         """
         Given a sentence, get its k nearest neighbours among the previously
         indexed sentences.
+
+        Arguments:
+            sentence:  string
+            k:         the number of nearest neighbours to return
+            type:      kNN algorithm, either 'dist' (default) or 'score'
+
+        Returns:
+            A list with two elements: the first element is a list with the k
+            nearest neighbour sentences (strings), and the second element is
+            a list of floats with the "distances" or "scores", respectively,
+            of the query sentence w.r.t. the k nearest neighbour sentences.
+            The latter depends on the value of 'type', i.e. 'dist' or 'score'.
         """
-        vector = self.__encoder.encode([sentence])
+        if type == "dist":
+            return self.__query_dist(sentence, k)
+        elif type == "score":
+            return self.__query_score(sentence, k)
+        else:
+            m = "'" + type +"' is not a valid argument for parameter 'type'."
+            raise Exception(m)
+
+    def __query_dist(self, sentence, k):
+        """
+        Query k nearest neighbours with direct distances between vectors.
+
+        The calculated metric is a distance where smaller means nearer.
+        """
+        vector = self.__encoder.encode([util.str2unicode(sentence)])
         distances = cdist(vector, self.__vectors)[0]
         k_indices = distances.argsort()[:k]
         k_sentences = map(util.unicode2str, self.__sentences[k_indices])
         k_distances = distances[k_indices].tolist()
         return [k_sentences, k_distances]
+
+    def __query_score(self, sentence, k):
+        """
+        Query k nearest neighbours with original skip-thoughts approach.
+
+        The calculated metric is a "score" where higher means nearer.
+        """
+        # Note: the following is taken from function "nn" in "skipthoughts.py"
+        vector = self.__encoder.encode([util.str2unicode(sentence)])
+        # Divide vector by scalar
+        vector /= norm(vector)
+        # Dot product of query vector with transposition of index vectors.
+        # Gives array of scalars, one for each index vector (highest is best).
+        scores = numpy.dot(vector, self.__vectors.T).flatten()
+        # Indices of k highest "scores" ([::-1] reverses order of array) 
+        k_indices = numpy.argsort(scores)[::-1][:k]
+        k_sentences = map(util.unicode2str, self.__sentences[k_indices])
+        k_scores = scores[k_indices].tolist()
+        return [k_sentences, k_scores]
