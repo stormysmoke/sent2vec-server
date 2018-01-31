744e3a78943a5b17c98f43fa2902acf6f7cb70e3
Daniel Weibel <danielmweibel@gmail.com>
2018-01-31 17:32:36 -0500

    Add output to temporary run script

--- tmp_run.py
+++ tmp_run.py
@@ -1,7 +1,18 @@
 from sent2vec.manager import Sent2VecManager
 
 sent2vec = Sent2VecManager.get_instance()
+
+print("Loading model:")
 sent2vec.load_model()
+
+print("Indexing sample text...")
 sent2vec.read_file("tmp_text.txt")
-sent2vec.query("The church had multiple origins.", 3, "dist")
-sent2vec.query("The church had multiple origins.", 3, "score")
+
+print("Result of sample 'dist' query:")
+l = sent2vec.query("The church had multiple origins.", 3, "dist")
+print(l)
+
+print("Result of sample 'score' query:")
+l = sent2vec.query("The church had multiple origins.", 3, "dist")
+l = sent2vec.query("The church had multiple origins.", 3, "score")
+print(l)
