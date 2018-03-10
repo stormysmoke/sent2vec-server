73b10b8b725cd21ca4bebd01098eef737a6500b4
Daniel Weibel <danielmweibel@gmail.com>
2018-03-10 17:29:16 -0500

    Add underline to private function names in main

--- main.py
+++ main.py
@@ -3,20 +3,24 @@ import sent2vec
 import saver
 import uuid
 
-def on_encode(text):
+"""
+Highest-level module and entry point to the application.
+"""
+
+def _on_encode(text):
     record = sent2vec.encode(text)
     id = str(uuid.uuid4())
     saver.put(id, record['sent'], record['vec'])
     return id
 
-def on_knn(query, k, id):
+def _on_knn(query, k, id):
     record = saver.get(id)
     return sent2vec.knn(query, record, k)
 
 sent2vec.init()
 communicator.init()
-communicator.register_on_encode(on_encode)
-communicator.register_on_knn(on_knn)
+communicator.register_on_encode(_on_encode)
+communicator.register_on_knn(_on_knn)
 
 # This call is blocking and must come last
 communicator.start_listening()
